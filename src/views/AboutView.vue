ebb8b76ef9390b3c4c8b9ee9c67236f03da55ed3
Errol Lin <errollin@qq.com>
2023-01-01 22:52:45 +0800

    Update AboutView page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,7 +1,37 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div>The About Page</div>
+  <div class="pa-6">
+    <h1>About Vuetify Todo</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi facere,
+      soluta unde ad hic, corrupti iure error at voluptatem nihil assumenda
+      asperiores nam maxime inventore, molestias et labore itaque velit?
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos iste rem
+      nihil culpa facere, repudiandae veritatis maxime ea aliquam quo veniam
+      nemo eum! Veniam alias eaque earum corporis labore ipsa?
+    </p>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam, dolores
+      eveniet. Eius amet aliquam perspiciatis aperiam corrupti esse hic
+      veritatis asperiores, ullam sunt dolores neque enim quos quidem alias
+      quod?
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil autem
+      aperiam facilis atque, necessitatibus temporibus expedita. Ea iusto harum
+      distinctio eaque voluptate illo labore, deleniti ducimus excepturi saepe
+      aut. Hic.
+    </p>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa iure
+      praesentium voluptates? Dolores, beatae, praesentium aspernatur nihil
+      quisquam illo omnis minima laudantium repellat repellendus distinctio ipsa
+      odio aliquid esse consequuntur?
+    </p>
+  </div>
 </template>
 
 <style lang="scss" scoped></style>
