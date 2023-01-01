b2c7bdeb3ca398614e245287e9c5d5a92e6cfe79
Errol Lin <errollin@qq.com>
2023-01-01 21:56:53 +0800

    Initialize project

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { reactive, toRefs } from "vue";
+
+const state = reactive({ drawer: false });
+
+const { drawer } = toRefs(state);
+</script>
+
+<template>
+  <v-app>
+    <v-navigation-drawer v-model="drawer">
+      <!--  -->
+    </v-navigation-drawer>
+
+    <v-app-bar>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <!--  -->
+    </v-main>
+  </v-app>
+</template>
+
+<style lang="scss"></style>
