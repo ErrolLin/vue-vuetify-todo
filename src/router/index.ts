9b3a970598414b84441169116a39c0d6d727efbe
Errol Lin <errollin@qq.com>
2023-01-01 22:49:49 +0800

    Add routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,18 @@
 import { createRouter, createWebHistory } from "vue-router";
 import type { RouteRecordRaw } from "vue-router";
 
-const routes: RouteRecordRaw[] = [];
+const routes: RouteRecordRaw[] = [
+  {
+    path: "/",
+    name: "Todo",
+    component: () => import("../views/TodoView.vue"),
+  },
+  {
+    path: "/about",
+    name: "About",
+    component: () => import("../views/AboutView.vue"),
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
